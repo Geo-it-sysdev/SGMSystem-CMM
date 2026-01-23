90a502c19a840c13e6c14e75e00a6111132386e6
geovannedaganato491-cloud <geovannedaganato491@gmail.com>
2026-01-23 15:11:33 +0800

    g

--- application/controllers/StudentController.php
+++ application/controllers/StudentController.php
@@ -766,6 +766,7 @@ public function update_student()
         $this->db->from('tbl_activities_header AS a');
         $this->db->join('tbl_activities_lines AS b', 'b.activities_id_header = a.id', 'left');
         $this->db->join('tbl_users AS c', 'c.id = a.user_id', 'left');
+        $this->db->where('b.section IS NOT NULL', null, false);
 
         if (!in_array($user_type, ['Principal', 'Registrar', 'Guidance Councilor', 'Admin'])) {
             $this->db->where('a.user_id', $user_id);

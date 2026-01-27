0fba90be0aa911fa730b05999ae6a258febd71b1
geovannedaganato491-cloud <geovannedaganato491@gmail.com>
2026-01-27 15:40:09 +0800

    g

--- application/controllers/StudentController.php
+++ application/controllers/StudentController.php
@@ -126,22 +126,24 @@ public function update_student()
     }
 
 
-    public function get_pending_students()
+    public function get_pending_students_by_grade()
     {
         $activity_id = $this->input->post('activity_id');
         $grade_level = $this->input->post('grade_level');
 
         $query = $this->db->query("
-            SELECT COUNT(c.id) AS pending_count
-            FROM tbl_students c
-            LEFT JOIN tbl_activities_lines b 
-                ON b.student_id = c.id
-            WHERE c.grade_level = ?
-            AND (b.activities_id_header IS NULL OR b.activities_id_header != ?)
-        ", [$grade_level, $activity_id]);
+            SELECT a.fullname, a.section, a.grade_level
+            FROM tbl_students AS a
+            LEFT JOIN tbl_activities_lines AS b 
+                ON b.student_id = a.id 
+                AND b.activities_id_header = ?
+            WHERE b.student_id IS NULL
+            AND a.grade_level = ?
+        ", [$activity_id, $grade_level]);
 
-        echo json_encode($query->row());
+        echo json_encode($query->result());
     }
+
 
     // end add / edit / delete student
 
